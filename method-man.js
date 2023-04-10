4b4dfd895c0c0ee0ff5546f72cfec9810f3e4866
maggieeagle <oryol.margo@gmail.com>
2023-04-10 13:49:40 +0300

    .

---
+++ method-man.js
@@ -0,0 +1,25 @@
+function words(s) {
+    return s.split(' ')
+}
+
+function sentence(arr){
+    return arr.join()
+}
+
+function yell(s){
+    return s.toUpperCase()
+}
+
+function whisper(s) {
+    return '*' + s.toLowerCase() + '*'
+}
+
+function capitilize(s) {
+    return s[0].toUpperCase() + s.substr(1, s.length - 1)
+}
+
+console.log(words("Hello world !"))
+console.log(sentence(['Hello', 'world', '!']))
+console.log(yell("Hello world !"))
+console.log(whisper("Hello world !"))
+console.log(capitilize("hello world !"))
