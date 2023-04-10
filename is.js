d61d4434bda0ac39a733a4f46e0588e1cb46623c
maggieeagle <oryol.margo@gmail.com>
2023-04-10 16:18:47 +0300

    .

---
+++ is.js
@@ -0,0 +1,35 @@
+const is = {}
+
+is.num = function(n) {
+    return typeof n == 'number' ? true : false
+}
+is.nan = function(n) {
+    return typeof n != 'number' ? true : false
+}
+is.str = function(n) {
+    return typeof n == 'string' ? true : false
+}
+is.bool = function(n) {
+    return typeof n == 'boolean' ? true : false
+}
+is.undef = function(n) {
+    return typeof n == 'undefined' ? true : false
+}
+is.def = function(n) {
+    return typeof n != 'undefined' ? true : false
+}
+is.arr = function(n) {
+    return Array.isArray(n) ? true : false
+}
+is.obj = function(n) {
+    return typeof n == 'object' ? true : false
+}
+is.fun = function(n) {
+    return n instanceof Function ? true : false
+}
+is.truthy = function(n) {
+    return n ? true : false
+}
+is.falsy = function(n) {
+    return !n ? true : false
+}
