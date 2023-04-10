2f33a92a7013028276440ca10e062c5d9536e774
maggieeagle <oryol.margo@gmail.com>
2023-04-10 11:41:16 +0300

    .

--- declarations.js
+++ declarations.js
@@ -4,5 +4,7 @@ const obj = {str: '', num: 0, bool: false, undef: undefined}
 const nested = {arr: [4, undefined, '2'], obj: {str: '', num: 0, bool: false}}
 
 Object.freeze(nested);
+Object.freeze(nested.arr);
+Object.freeze(nested.obj);
 Object.freeze(arr);
 Object.freeze(obj);
