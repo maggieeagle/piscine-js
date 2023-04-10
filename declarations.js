bf17f3e5322f976d7c3412c8f1f09fe18aee4bc3
maggieeagle <oryol.margo@gmail.com>
2023-04-10 11:30:32 +0300

    .

--- declarations.js
+++ declarations.js
@@ -1,2 +1,8 @@
 const escapeStr = "`\\/\"'"
-console.log(escapeStr)
+const arr = [2, '2']
+const obj = {str: '', num: 0, bool: false, undef: undefined}
+const nested = {arr: [4, undefined, '2'], obj: {str: '', num: 0, bool: false}}
+
+Object.freeze(nested);
+Object.freeze(arr);
+Object.freeze(obj);
