bd56e43b8c19bbdaa4a6709030f361e596656478
maggieeagle <oryol.margo@gmail.com>
2023-04-12 18:10:16 +0300

    .

--- cut-corners.js
+++ cut-corners.js
@@ -1,28 +1,30 @@
-function round(n0) {
-    if (isNaN(n0)) return 'Error: Nan is not defined'
-    let i = 0, n = Math.abs(n0)
-    if (n == Infinity) return Math.sign(n0) * Infinity
-    for (; i < n; i++) {}
-    return i - n > n - i + 1 ? Math.sign(n0) * (i-1) : Math.sign(n0) * i
+function round(n) {
+    let i = 0, sign = Math.sign(n)
+    if (n == sign * Infinity) return n
+    if (sign >= 0) {
+        for (; i < n; i++) {}
+        return i-n > n-i+1 ? i-1 : i
+    }
+    for (; i > n; i--) {}
+    let res = i-n > n-i-1 ? i : i+1
+    return res == 0 ? -0 : res
 }
 
 function floor(n0) {
-    if (isNaN(n0)) return 'Error: Nan is not defined'
-    let i = 0, n = Math.abs(n0)
-    if (n == Infinity) return Math.sign(n0) * Infinity
+    let i = 0, n = Math.abs(n0), sign = Math.sign(n0)
+    if (n == Infinity) return n0
     for (; i < n; i++) {}
-    return Math.sign(n0) < 1 ? (i != 0 ? -i : 0) : i-1
+    if (i == n0) return n
+    return sign < 1 ? (i != 0 ? -i : sign * 0) : i-1
 }
 
 function trunc(n) {
     return Math.sign(n) < 0 ? ceil(n) : floor(n)
 }
 function ceil(n0) {
-    if (isNaN(n0)) return 'Error: Nan is not defined'
-    let i = 0, n = Math.abs(n0)
-    if (n == Infinity) return Math.sign(n0) * Infinity
+    let i = 0, n = Math.abs(n0), sign = Math.sign(n0)
+    if (n == Infinity) return n0
     for (; i < n; i++) {}
-    return Math.sign(n0) < 0 ? -i+1 : i
+    if (-i == n0) return n
+    return sign < 1 ? (-i+1 != 0 ? -i+1 : sign * 0) : i
 }
-
-console.log(trunc(NaN))
