41524e6ea785d5eac7676e00c4bb564abb386b0c
maggieeagle <oryol.margo@gmail.com>
2023-04-12 14:57:07 +0300

    .

--- cut-corners.js
+++ cut-corners.js
@@ -1,12 +1,10 @@
 function round(n0) {
-    let i = 0, n = Math.abs(n0)
-    for (; i < n; i++) {}
+    let n = Math.abs(n0), i = parseInt(n) + 1
     return i - n > n - i + 1 ? Math.sign(n0) * (i-1) : Math.sign(n0) * i
 }
 
 function floor(n0) {
-    let i = 0, n = Math.abs(n0)
-    for (; i < n; i++) {}
+    let n = Math.abs(n0), i = parseInt(n) + 1
     return Math.sign(n0) < 1 ? (i != 0 ? -i : 0) : i-1
 }
 
@@ -14,7 +12,6 @@ function trunc(n) {
     return Math.sign(n) < 0 ? ceil(n) : floor(n)
 }
 function ceil(n0) {
-    let i = 0, n = Math.abs(n0)
-    for (; i < n; i++) {}
+    let n = Math.abs(n0), i = parseInt(n) + 1
     return Math.sign(n0) < 0 ? -i+1 : i
 }
