0ee65c4661eb81ba873201ef48589a35e54e0e63
maggieeagle <oryol.margo@gmail.com>
2023-04-12 21:40:47 +0300

    .

---
+++ unbreakable.js
@@ -0,0 +1,17 @@
+function split(s) {
+    let res = []
+    for (let i = 0; i < s.length; i++) {
+        res.push(s[i])
+    }
+    return res
+}
+
+function join(arr) {
+    let res = ''
+    for (let i = 0; i < arr.length; i++) {
+        res += arr[i]
+    }
+    return res
+}
+
+console.log(join([ "h", "e", "l", "l", "o" ]))
