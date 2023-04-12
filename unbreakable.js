f14e4ab9eb7586c06408f85c47169118d1bebac2
maggieeagle <oryol.margo@gmail.com>
2023-04-13 00:43:12 +0300

    .

--- unbreakable.js
+++ unbreakable.js
@@ -1,15 +1,21 @@
 function split(s, sep) {
-    let res = [], prev = 0, next = 0
-    const sepLen = sep.length
-    while(next + sepLen <= s.length) {
-        let next = s.indexOf(sep, prev)
-        if (next == -1) {
-            res.push(s.substring(next == 0 ? 0 : prev, s.length))
-            break;
+    let res = []
+    if (!(sep == null || sep == '')) {
+        let prev = 0, next = 0
+        while(next + sep.length <= s.length) {
+            let next = s.indexOf(sep, prev)
+            if (next == -1) {
+                res.push(s.substring(next == 0 ? 0 : prev, s.length))
+                break;
+            }
+            let slice = s.substring(prev == 0 ? 0 : prev, next)
+            res.push(slice)
+            prev = next + sep.length
         }
-        let slice = s.substring(prev == 0 ? 0 : prev, next)
-        res.push(slice)
-        prev = next + sepLen
+    } else {
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] != ' ') res.push(s[i])
+        }
     }
     return res
 }
@@ -29,4 +35,5 @@ console.log(split('ee,ff,g,', ','))
 console.log(split('Riad', ' '))
 console.log(split('rrrr', 'rr'))
 console.log(split('rrirr', 'rr'))
+console.log(split('hello', ''))
 console.log(join(['Fire', 'Air', 'Water'], '-'))
