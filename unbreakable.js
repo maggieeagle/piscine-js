ad985201f176284e49be7a67f19153ee272a7865
maggieeagle <oryol.margo@gmail.com>
2023-04-12 22:21:22 +0300

    .

--- unbreakable.js
+++ unbreakable.js
@@ -1,17 +1,34 @@
-function split(s) {
-    let res = []
+function split(s, sep) {
+    let res = [], prev = -1, word = ''
     for (let i = 0; i < s.length; i++) {
-        if (s[i] != ' ') res.push(s[i])
+        let isSep = ''
+        for (let j = i - sep.length + 1; j <= i; j++) {
+            isSep += s[j]
+        }
+        if (isSep == sep) {
+            for (let j = prev + 1; j < i - sep.length + 1; j++) {
+                word += s[j]
+            }
+            prev = i
+            if (word != '') res.push(word)
+            word = ''
+        }
     }
+    for (let j = prev + 1; j < s.length; j++) {
+        word += s[j]
+    }
+    res.push(word)
     return res
 }
 
-function join(arr) {
+function join(arr, sep) {
     let res = ''
     for (let i = 0; i < arr.length; i++) {
         res += arr[i]
+        if (i < arr.length-1) res += sep
     }
     return res
 }
 
-console.log(join([ "h", "e", "l", "l", "o" ]))
+console.log(split('bla bla bla bla', 'bl'))
+console.log(join(['Fire', 'Air', 'Water'], ''))
