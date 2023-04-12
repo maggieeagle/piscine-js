7c2251c970cb3231f3fe70e00069e6554071d406
maggieeagle <oryol.margo@gmail.com>
2023-04-12 23:41:19 +0300

    .

--- unbreakable.js
+++ unbreakable.js
@@ -7,7 +7,7 @@ function split(s, sep) {
             // console.log('next!', next)
             // console.log('s.length-1!', s.length-1)
             // console.log("slice", s.slice(next+1, s.length))
-            res.push(s.slice(next+sep.length-1, s.length))
+            res.push(s.slice(next+sep.length, s.length))
             break;
         }
         next = tmp
@@ -37,4 +37,5 @@ function join(arr, sep) {
 }
 
 console.log(split('ggg - ddd - b', ' - '))
+console.log(split('a b c', ' '))
 // console.log(join(['Fire', 'Air', 'Water'], ''))
