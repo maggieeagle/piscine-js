1a32d3367d15690d4a9e9099fc6793c61482dc40
maggieeagle <oryol.margo@gmail.com>
2023-04-12 13:05:31 +0300

    .

--- slicer.js
+++ slicer.js
@@ -1,14 +1,18 @@
 function slice(data, first, last) {
+    let flag = false
     if (typeof data == 'string') {
         data = Array.from(data)
+        flag = true
     }
     let res = [], len = data.length
     for (let i = setIndex(len, first); i < (last == null ? len :  setIndex(len, last)); i++) {
         res.push(data[i])
     }
-    return res
+    return flag ? res.join('') : res
 }
 
 function setIndex(len, i) {
     return i < 0 ? len + i : i
 }
+
+console.log(slice('abcdef', 2))
