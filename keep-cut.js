a68228e6e24938c172af53c872080e302cb6c6ba
maggieeagle <oryol.margo@gmail.com>
2023-04-12 13:31:42 +0300

    .

---
+++ keep-cut.js
@@ -0,0 +1,23 @@
+function cutFirst(s) {
+    return s.slice(2)
+}
+
+function cutLast(s) {
+    return s.slice(0, s.length-2)
+}
+
+function cutFirstLast(s) {
+    return s.slice(2, s.length-2)
+}
+
+function keepFirst(s) {
+    return s.slice(0, 2)
+}
+
+function keepLast(s) {
+    return s.slice(s.length-2, s.length)
+}
+
+function keepFirstLast(s) {
+    return keepFirst(s) + keepLast(s)
+}
