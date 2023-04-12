4dcc014e06fbdf4a2d7800cde0315cfbc0c70c1e
maggieeagle <oryol.margo@gmail.com>
2023-04-12 20:44:06 +0300

    .

--- cut-corners.js
+++ cut-corners.js
@@ -1,8 +1,8 @@
 function round(n) {
     let i = 0, sign = Math.sign(n)
-    if (n == sign * Infinity) return n
     if (Number.isInteger(n)) return n
-    if (n > Number.MAX_VALUE || n < -Number.MAX_VALUE) return n
+    if (n == Number.POSITIVE_INFINITY || n == Number.NEGATIVE_INFINITY) return n
+    // if (n > Number.MAX_VALUE || n < -Number.MAX_VALUE) return n
     if (sign >= 0) {
         for (; i < n; i++) { }
         return i - n > n - i + 1 ? i - 1 : i
@@ -14,9 +14,9 @@ function round(n) {
 
 function floor(n0) {
     let i = 0, n = Math.abs(n0), sign = Math.sign(n0)
-    if (n == Infinity) return n0
     if (Number.isInteger(n)) return n
-    if (n0 > Number.MAX_VALUE || n0 < -Number.MAX_VALUE) return n0
+    if (n0 == Number.POSITIVE_INFINITY || n0 == Number.NEGATIVE_INFINITY) return n0
+    // if (n0 > Number.MAX_VALUE || n0 < -Number.MAX_VALUE) return n0
     for (; i < n; i++) { }
     return sign < 1 ? (i != 0 ? -i : sign * 0) : i - 1
 }
@@ -27,9 +27,9 @@ function trunc(n) {
 
 function ceil(n0) {
     let i = 0, n = Math.abs(n0), sign = Math.sign(n0)
-    if (n == Infinity) return n0
     if (Number.isInteger(n)) return n
-    if (n0 > Number.MAX_VALUE || n0 < -Number.MAX_VALUE) return n0
+    if (n0 == Number.POSITIVE_INFINITY || n0 == Number.NEGATIVE_INFINITY) return n0
+    // if (n0 > Number.MAX_VALUE || n0 < -Number.MAX_VALUE) return n0
     for (; i < n; i++) { }
     if (-i == n0) return n
     return sign < 1 ? (-i + 1 != 0 ? -i + 1 : sign * 0) : i
