a688a7fbf94b6740f34e2595b2cf904beed4f24e
maggieeagle <oryol.margo@gmail.com>
2023-04-12 22:46:55 +0300

    .

--- unbreakable.js
+++ unbreakable.js
@@ -1,24 +1,21 @@
 function split(s, sep) {
-    let res = [], prev = -1, word = ''
+    let res = [], prev = 0
     for (let i = 0; i < s.length; i++) {
-        let isSep = ''
-        for (let j = i - sep.length + 1; j <= i; j++) {
-            isSep += s[j]
-        }
+        let isSep = s.slice(i - sep.length + 1, i + 1)
+        console.log("sep", isSep)
         if (isSep == sep) {
             // for (let j = prev + 1; j < i - sep.length + 1; j++) {
             //     word += s[j]
             // }
-            res.push(s.slice(prev, i-sep.length))
+            res.push(s.slice(prev, i - sep.length + 1))
             prev = i
-            word = ''
-            i += sep.length-1
+            i += sep.length - 1
         }
     }
-    for (let j = prev + 1; j < s.length; j++) {
-        word += s[j]
-    }
-    res.push(word)
+    // for (let j = prev + 1; j < s.length; j++) {
+    //     word += s[j]
+    // }
+    res.push(s.slice(prev, s.length + 1))
     return res
 }
 
@@ -26,10 +23,10 @@ function join(arr, sep) {
     let res = ''
     for (let i = 0; i < arr.length; i++) {
         res += arr[i]
-        if (i < arr.length-1) res += sep
+        if (i < arr.length - 1) res += sep
     }
     return res
 }
 
-console.log(split('bla bla bla bla', 'bl'))
+console.log(split('a b c', ' '))
 console.log(join(['Fire', 'Air', 'Water'], ''))
