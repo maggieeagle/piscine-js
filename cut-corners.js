93cc93ea9e6efb843bc038889eab7cc21b1349d3
maggieeagle <oryol.margo@gmail.com>
2023-04-12 18:28:42 +0300

    .

--- cut-corners.js
+++ cut-corners.js
@@ -2,20 +2,20 @@ function round(n) {
     let i = 0, sign = Math.sign(n)
     if (n == sign * Infinity) return n
     if (sign >= 0) {
-        for (; i < n; i++) {}
-        return i-n > n-i+1 ? i-1 : i
+        for (; i < n; i++) { }
+        return i - n > n - i + 1 ? i - 1 : i
     }
-    for (; i > n; i--) {}
-    let res = i-n > n-i-1 ? i : i+1
+    for (; i > n; i--) { }
+    let res = i - n > n - i - 1 ? i : i + 1
     return res == 0 ? -0 : res
 }
 
 function floor(n0) {
     let i = 0, n = Math.abs(n0), sign = Math.sign(n0)
     if (n == Infinity) return n0
-    for (; i < n; i++) {}
+    for (; i < n; i++) { }
     if (i == n0) return n
-    return sign < 1 ? (i != 0 ? -i : sign * 0) : i-1
+    return sign < 1 ? (i != 0 ? -i : sign * 0) : i - 1
 }
 
 function trunc(n) {
@@ -24,8 +24,9 @@ function trunc(n) {
 function ceil(n0) {
     let i = 0, n = Math.abs(n0), sign = Math.sign(n0)
     if (n == Infinity) return n0
-    for (; i < n; i++) {}
+    for (; i < n; i++) { }
     if (-i == n0) return n
-    return sign < 1 ? (-i+1 != 0 ? -i+1 : sign * 0) : i
+    return sign < 1 ? (-i + 1 != 0 ? -i + 1 : sign * 0) : i
 }
+
 
