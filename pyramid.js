f1f69f7217494091b3213c22caccba15f585465f
maggieeagle <oryol.margo@gmail.com>
2023-04-13 12:09:29 +0300

    .

--- pyramid.js
+++ pyramid.js
@@ -1,8 +1,10 @@
 function pyramid(s, n) {
     let res = ''
     for (let i = 1; i <= n; i++) {
-        res += (' ').repeat(n-i) + s.repeat(2*i-1)
+        res += (' ').repeat((n-i)*s.length) + s.repeat(2*i-1)
         if  (i < n) res += '\n'
     }
     return res
 }
+
+console.log(pyramid('{}', 12))
