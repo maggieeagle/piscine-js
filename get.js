77d23549fab8d5e2c5abc4a1e84246ad34935794
maggieeagle <oryol.margo@gmail.com>
2023-04-13 11:46:55 +0300

    .

--- get.js
+++ get.js
@@ -1,7 +1,12 @@
 function get(src, path) {
     let a = path.split('.'), current = src
     for (let i = 0; i < a.length; i++) {
-        current = current[a[i]]
+        if (a[i] in current) current = current[a[i]]
+        else return undefined
     }
     return current
 }
+
+const src = { nested: { key: 'peekaboo' } }
+const path = 'nested.key'
+console.log(get({ nested: { key: 'value' } }, 'nx.nx'))
