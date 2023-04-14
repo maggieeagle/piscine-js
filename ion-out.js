efc2b94e536893b374c965e1057023e4c6b520ab
maggieeagle <oryol.margo@gmail.com>
2023-04-14 17:23:34 +0300

    .

--- ion-out.js
+++ ion-out.js
@@ -1,8 +1,10 @@
 function ionOut(s) {
-    let input  = s.split(', '), res = []
+    let input  = s.split(/,?\s+/), res = []
     input.forEach(word => {
         let re = /(?<=t)ion/
         if (re.test(word)) res.push(word.replace(re, '')) 
     });
     return res
 }
+
+console.log(ionOut(' 1st position is the vision of the 2nd position'))
