237a42cdb4f464e1652ab2cae94db0933480d701
maggieeagle <oryol.margo@gmail.com>
2023-04-14 21:50:38 +0300

    .

--- group-price.js
+++ group-price.js
@@ -2,9 +2,9 @@ function groupPrice(s) {
     let re = /(?<=\s)[a-zA-Z|$]+[\d]+(\.([\d]*))?/g
     let matches = s.match(re), res = []
     matches.forEach(element => {
-        res.push([element, element.match(/(?<=[\w|$])\d*(?=\.)/), element.match(/(?<=\.)\d*/)])
+        res.push([element, element.match(/(?<=[\w|$])\d*(?=\.)/)[0], element.match(/(?<=\.)\d*/)[0]])
     });
     return res
 }
 
-console.log(groupPrice('The price of the cereals is $4.00.  the total is USD19.98'))
+console.log(groupPrice('The price of the cereals is $4.00'))
