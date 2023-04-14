e65ae046487b17a7dd1bc0fa7a0a6a9abad80c20
maggieeagle <oryol.margo@gmail.com>
2023-04-14 16:28:39 +0300

    .

--- same-amount.js
+++ same-amount.js
@@ -1,3 +1,7 @@
 function sameAmount(s, re1, re2) {
-    return s.match(re1).length == s.match(re2).length ? true : false
+    let fir = s.match(re1)
+    let sec = s.match(re2)
+    return fir == null || sec == null || fir.length != s.match(re2).length ? false : true
 }
+
+console.log(sameAmount('kjdnkruhikejrbjhb', /h/, /w/))
