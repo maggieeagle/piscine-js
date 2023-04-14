c18ef173eac1d14d6f19c7227c071006f1c161ad
maggieeagle <oryol.margo@gmail.com>
2023-04-14 23:21:15 +0300

    .

--- valid-ip.js
+++ valid-ip.js
@@ -1,31 +1,29 @@
 function findIP(s) {
-    let re = /([123456789]{1}\d{0,2})\.([123456789]{1}\d{0,2})\.([123456789]{1}\d{0,2})\.([123456789]{1}\d{0,2})(:(\d*))?/g
+    let re = /(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})(:(\d*))?/g
     let matches = s.match(re), res = []
     if (matches == null) return []
     for (let i = 0; i < matches.length; i++) {
         const match = matches[i];
         // console.log('match', match)
-        const groups = re.exec(match);
+        let re1 = /(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})(:(\d*))?/
+        const groups = re1.exec(match);
         // console.log(groups);
-        if (groups.length[5] == undefined) {
-            if (groups[1] > 0 && groups[1] < 255 &&
-                groups[2] > 0 && groups[2] < 255 &&
-                groups[2] > 0 && groups[2] < 255 &&
-                groups[3] > 0 && groups[3] < 255) {
+        if (groups[6] == undefined) {
+            if ((groups[1][0] != 0 || groups[1] == '0') && groups[1] >= 0 && groups[1] <= 255 &&
+                (groups[2][0] != 0 || groups[2] == '0') && groups[2] >= 0 && groups[2] <= 255 &&
+                (groups[3][0] != 0 || groups[3] == '0') && groups[3] >= 0 && groups[3] <= 255 &&
+                (groups[4][0] != 0 || groups[4] == '0') && groups[4] >= 0 && groups[4] <= 255) {
                 res.push(match)
             }
         } else {
-            if (groups[1] > 0 && groups[1] < 255 &&
-                groups[2] > 0 && groups[2] < 255 &&
-                groups[2] > 0 && groups[2] < 255 &&
-                groups[3] > 0 && groups[3] < 255 &&
-                groups[5] > 0 && groups[5] < 65535) {
+            if ((groups[1][0] != 0 || groups[1] == '0') && groups[1] >= 0 && groups[1] <= 255 &&
+                (groups[2][0] != 0 || groups[2] == '0') && groups[2] >= 0 && groups[2] <= 255 &&
+                (groups[3][0] != 0 || groups[3] == '0') && groups[3] >= 0 && groups[3] <= 255 &&
+                (groups[4][0] != 0 || groups[4] == '0') && groups[4] >= 0 && groups[4] <= 255 &&
+                groups[6] > 0 && groups[6] <= 65535) {
                 res.push(match)
             }
         }
     }
     return res == null ? [] : res
 }
-
-// let data = '55.234.100.20:34555'
-// console.log(findIP(data))
