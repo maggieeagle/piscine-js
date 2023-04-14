74f3a9b6ce2504409e776f0ec368d4819711d51b
maggieeagle <oryol.margo@gmail.com>
2023-04-14 17:19:29 +0300

    .

--- ion-out.js
+++ ion-out.js
@@ -2,7 +2,7 @@ function ionOut(s) {
     let input  = s.split(', '), res = []
     input.forEach(word => {
         let re = /(?<=t)ion/
-        if (re.test(s)) res.push(word.replace(re, '')) 
+        if (re.test(word)) res.push(word.replace(re, '')) 
     });
     return res
 }
