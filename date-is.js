5098ae436fc57533abfd7a0e8f9e8924c4113f8d
maggieeagle <oryol.margo@gmail.com>
2023-04-18 14:26:50 +0300

    .

---
+++ date-is.js
@@ -0,0 +1,36 @@
+function isValid(date) {
+    if (date.pasre() == NaN) return false
+    return true
+}
+
+function isAfter(date1, date2) {
+    if(isValid(date1) && isValid(date2)) {
+        if (date1.getTime() > date2.getTime()) return true
+        return false
+    }
+    return false
+}
+
+function isBefore(date1, date2) {
+    if(isValid(date1) && isValid(date2)) {
+        if (date1.getTime() < date2.getTime()) return true
+        return false
+    }
+    return false
+}
+
+function isFuture(date) {
+    if(isValid(date)) {
+        if (date.getTime() > new Date.now()) return true
+        return false
+    }
+    return false
+}
+
+function isPast(date) {
+    if(isValid(date)) {
+        if (date.getTime() < new Date.now()) return true
+        return false
+    }
+    return false
+}
