9f22c4dbabd8b06e24b56d140b8778c42673c5d9
maggieeagle <oryol.margo@gmail.com>
2023-04-18 19:03:06 +0300

    .

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,6 +1,8 @@
 function dayOfTheYear(d) {
-    let date = new Date(d.getFullYear(), 1, 1)
-    return d.getDate() - date.getDate() + 1
+    let date = new Date(d.toISOString().split('T')[0].split('-')[0] + '-01-01T00:00:00.000Z')
+    console.log(date)
+    return Math.ceil((d.getTime() - date.getTime())/(24*3600*1000)) + 1
 }
 
-// console.log(dayOfTheYear(new Date('0001-01-01')))
+console.log(dayOfTheYear(new Date('0001-01-01')))
+console.log(dayOfTheYear(new Date('1664-08-09')))
