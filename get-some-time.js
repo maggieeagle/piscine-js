3c89242b52152dd240c78a2152db75c929e7387c
maggieeagle <oryol.margo@gmail.com>
2023-04-18 16:10:09 +0300

    .

--- get-some-time.js
+++ get-some-time.js
@@ -2,7 +2,7 @@ function firstDayWeek(week, year) {
     let date = new Date(year+'-01-01T00:00:00.000Z'), date2 = new Date(year+'-01-01T00:00:00.000Z')
     console.log(date)
     console.log('day', date.getDay())
-    date2.setDate(date.getDate() + (week-1)*7 - (date.getDay()+1))
+    date2.setDate(date.getDate() + (week-1)*7 - (date.getDay()-1))
     console.log(date2.getTime(), date.getTime())
     if (date2.getTime() < date.getTime()) {
         let s = date.toISOString().split('T')[0]
@@ -12,4 +12,4 @@ function firstDayWeek(week, year) {
     return s.split('-').reverse().join('-')
 }
 
-console.log(firstDayWeek(1, '1000'))
+console.log(firstDayWeek(52, '1000'))
