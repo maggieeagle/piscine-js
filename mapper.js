325a1e13c69b915add3378a4af22def35d66a11a
maggieeagle <oryol.margo@gmail.com>
2023-04-19 16:31:31 +0300

    .

--- mapper.js
+++ mapper.js
@@ -9,3 +9,15 @@ function map(arr, f) {
 function flatMap(arr, f) {
     return map(arr, f).flat(1)
 }
+
+function flat(arr, depth) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i]) && (depth > 0 || depth == null)) {
+            res = res.concat(flat(arr[i], depth-1))
+        } else {
+            res.push(arr[i])
+        }
+    }
+    return res
+}
