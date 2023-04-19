41f6c528b80cd8758389a4750611a52c58da7a4f
maggieeagle <oryol.margo@gmail.com>
2023-04-19 21:49:24 +0300

    .

---
+++ currify.js
@@ -0,0 +1,17 @@
+function currify(func) {
+    return function curried(...args){
+        if (args.length >= func.length) {
+            return func.apply(null, args)
+        }
+        return function(...args2) {
+            return curried.apply(null, args.concat(args2))
+        }
+    }
+}
+
+// const mult2 = (el1, el2) => el1 * el2
+// console.log(mult2(2, 2)) // result expected 4
+
+// const mult2Curried = currify(mult2)
+
+// console.log(mult2Curried(2)(2))
