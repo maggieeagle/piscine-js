b1dafe525c55b4a8b1c9dba98311c20d57c5d27d
maggieeagle <oryol.margo@gmail.com>
2023-04-19 16:21:27 +0300

    .

--- mapper.js
+++ mapper.js
@@ -1,7 +1,7 @@
 function map(arr, f) {
     let res=[]
     for(let i = 0; i < arr.length; i++){
-        res.push(f(arr[i], i))
+        res.push(f(arr[i], i, arr))
     }
     return res
 }
@@ -9,3 +9,8 @@ function map(arr, f) {
 function flatMap(arr, f) {
     return map(arr, f).flat(1)
 }
+
+const indexValsArray = (el, i, arr) =>
+  `${el} is at index: ${i} out of ${arr.length - 1}`
+  const numbers = [10, -10, 20, -95, 86, 102, 35, 89, 110]
+console.log(map(numbers, indexValsArray))
