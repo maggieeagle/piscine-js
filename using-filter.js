882d3b8dcd86fb65bd08584051c5949b007f98db
maggieeagle <oryol.margo@gmail.com>
2023-04-19 13:56:27 +0300

    .

---
+++ using-filter.js
@@ -0,0 +1,25 @@
+function filterShortDtateName(arr) {
+    return arr.filter(word => word.length < 7)
+}
+
+function filterStartVowel(arr) {
+    re = /^[a|e|i|o|u|A|E|I|O|U]/
+    return arr.filter(word => re.test(word))
+}
+
+function filter5Vowels(arr) {
+    re = /[a|e|i|o|u|A|E|I|O|U]/g
+    return arr.filter(word => word.match(re).length >= 5)
+}
+
+function filter1DistinctVowel(arr) {
+    re = /(?<=[^a|^e|^i|^o|^u|^A|^E|^I|^O|^U])[a|e|i|o|u|A|E|I|O|U](?=[^a|^e|^i|^o|^u|^A|^E|^I|^O|^U])/
+    return arr.filter(word => re.test(word))
+}
+
+function multiFilter(arr) {
+    let re = /[a|e|i|o|u|A|E|I|O|U]/
+    return arr.filter(item => item.capital != null && item.capital.length >= 8 && filterStartVowel([item.name]) == [] && re.test(item.tag) && item.region != 'South')
+}
+
+console.log(filter1DistinctVowel(['alabama', 'apple', 'bear', 'euiofjshiie']))
