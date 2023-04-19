e956f185c267b0c1720757a3889a97a7b3002f0e
maggieeagle <oryol.margo@gmail.com>
2023-04-19 16:53:18 +0300

    .

---
+++ filter.js
@@ -0,0 +1,24 @@
+function filter(arr, f) {
+    let res=[]
+    for(let i = 0; i < arr.length; i++){
+        if (f(arr[i])) res.push(arr[i])
+    }
+    return res
+}
+
+function filter(arr, f) {
+    let res=[]
+    for(let i = 0; i < arr.length; i++){
+        if (!f(arr[i])) res.push(arr[i])
+    }
+    return res
+}
+
+function filter(arr, f) {
+    let res1=[], res2=[]
+    for(let i = 0; i < arr.length; i++){
+        if (f(arr[i])) res1.push(arr[i])
+        else res2.push(arr[i])
+    }
+    return [].push(res1).push(res2)
+}
