ce121a49e89de7c74e85a230c1009579ffdac19b
maggieeagle <oryol.margo@gmail.com>
2023-04-19 14:30:38 +0300

    .

--- using-filter.js
+++ using-filter.js
@@ -13,8 +13,8 @@ function filter5Vowels(arr) {
 }
 
 function filter1DistinctVowel(arr) {
-    let re = /(?<=[^a|^e|^i|^o|^u|^A|^E|^I|^O|^U])[a|e|i|o|u|A|E|I|O|U](?=[^a|^e|^i|^o|^u|^A|^E|^I|^O|^U])/
-    return arr.filter(word => re.test(word))
+    let re = /[a|e|i|o|u|A|E|I|O|U]/g
+    return arr.filter(word => re.test(word) && word.match(re).filter(x => x.toLowerCase() == word.match(re)[0].toLowerCase()).length == word.match(re).length);
 }
 
 function multiFilter(arr) {
@@ -22,4 +22,55 @@ function multiFilter(arr) {
     return arr.filter(item => item.capital != null && item.capital.length >= 8 && filterStartVowel([item.name]) == [] && re.test(item.tag) && item.region != 'South')
 }
 
-console.log(filter1DistinctVowel(['alabama', 'apple', 'bear', 'euiofjshiie']))
+console.log(filter1DistinctVowel([
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Carolina',
+    'North Dakota',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virginia',
+    'Washington',
+    'West Virginia',
+    'Wisconsin',
+    'Wyoming',
+  ]))
