b744a6f43936719931d0d1fa34df679210b0445d
maggieeagle <oryol.margo@gmail.com>
2023-04-19 15:35:25 +0300

    .

--- for-each.js
+++ for-each.js
@@ -1,5 +1,11 @@
 function forEach(arr, f) {
     for (let i = 0; i<arr.length; i++){
-        f(arr[i])
+        f(arr[i], i)
     }
 }
+
+const arr = [1, 2, 3, 4, 5, Math.random(), 7, 10, -10, 20, -95]
+
+const result = []
+forEach(arr, (_, index) => result.push(index))
+console.log(result)
