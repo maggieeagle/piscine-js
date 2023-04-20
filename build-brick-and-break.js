02eebcab7bd878bb8237b8042871895615f8eedc
maggieeagle <oryol.margo@gmail.com>
2023-04-20 16:45:19 +0300

    .

---
+++ build-brick-and-break.js
@@ -0,0 +1,28 @@
+export const build = (n) => {
+    for (let i = 1; i <= n; i++) {
+        setTimeout(function () {
+            const template = document.createElement('div');
+            template.setAttribute('id', 'brick-' + i);
+            template.innerHTML = "";
+
+            if (n % 2 == 0) {
+                template.setAttribute('foundation', true);
+            }
+            document.body.appendChild(template);
+        }, 100 * i);
+    }
+}
+
+export const repair = (...ids) => {
+    ids.forEach(id => {
+        console.log(id)
+        let brick = document.getElementById(id)
+        if (id%2 == 0) brick.setAttribute('repaired', 'in progress')
+        else brick.setAttribute('repaired', true)
+    });
+}
+
+export const destroy = () => {
+    let last = document.body.lastChild
+    document.body.removeChild(last)
+}
