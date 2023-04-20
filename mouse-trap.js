64cf9c6f8ae5d54ab9c4ffea9c542c2fda9f354e
maggieeagle <oryol.margo@gmail.com>
2023-04-20 23:57:10 +0300

    .

--- mouse-trap.js
+++ mouse-trap.js
@@ -32,8 +32,8 @@ export const moveCircle = () => {
         let trap = box.getBoundingClientRect();
         let top = y-e.offsetHeight/2, right = x + e.offsetWidth/2, bottom = y + e.offsetHeight/2, left = x-e.offsetWidth/2
         if (!e.classList.contains('trapped')) return true
-        else if (top > trap.top + 1 && left > trap.left + 1 &&
-            bottom < trap.bottom - 1 && right < trap.right - 1) return true
+        else if (top > trap.top && left > trap.left &&
+            bottom < trap.bottom && right < trap.right) return true
         return false
     }
 }
