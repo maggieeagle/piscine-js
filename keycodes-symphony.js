9d5ca9267e455bd94fc06e27ae10de5a20f28bce
maggieeagle <oryol.margo@gmail.com>
2023-04-20 21:58:39 +0300

    .

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -1,11 +1,11 @@
 export const compose = () => {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    const alphabet = ' abcdefghijklmnopqrstuvwxyz'
     document.addEventListener('keydown', (e) => {
         if (/^[a-zA-Z]\b/.test(e.key)) {
             const template = document.createElement('div');
             template.classList.add('note');
             template.innerHTML = e.key;
-            template.style.backgroundColor = '#'+Math.floor(alphabet.indexOf(e.key)*671088).toString(16);
+            template.style.backgroundColor = '#'+Math.floor(alphabet.indexOf(e.key)*645277).toString(16);
             document.body.appendChild(template);
         }
         if (e.key === 'Backspace') {
