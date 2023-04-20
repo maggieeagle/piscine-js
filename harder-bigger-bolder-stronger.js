6864cc3a498e750a331a2f6301f1710c19982a05
maggieeagle <oryol.margo@gmail.com>
2023-04-20 17:33:58 +0300

    .

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -4,7 +4,7 @@ export const generateLetters = () => {
         const template = document.createElement('div');
         template.innerHTML = alphabet[getRandomInt(alphabet.length)];
         template.style.fontSize = size + 'px'
-        template.style.fontWeight = 300 + 100*Math.floor(i/40)
+        template.style.fontWeight = i>80?600:300 + 100*Math.floor(i/40)
         document.body.appendChild(template);
     }
 
