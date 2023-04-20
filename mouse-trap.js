2bd0cb755b6ac06655cbdb6101a6a403fc3b4756
maggieeagle <oryol.margo@gmail.com>
2023-04-21 00:56:56 +0300

    .

--- mouse-trap.js
+++ mouse-trap.js
@@ -47,6 +47,6 @@ function trap(e, box) {
     if (circle.top > trap.top + 1 && circle.left > trap.left &&
         circle.bottom < trap.bottom && circle.right < trap.right) {
         e.classList.add('trapped')
-        e.style.backgroundColor = getComputedStyle(document.querySelector(':root')).getPropertyValue('--purple')
+        e.style.background = getComputedStyle(document.querySelector(':root')).getPropertyValue('--purple')
     }
 }
