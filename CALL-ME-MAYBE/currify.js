0046a823f87b58c485cd87249f068ccb7a41b435
maggieeagle <oryol.margo@gmail.com>
2023-04-20 14:06:41 +0300

    .

--- CALL-ME-MAYBE/currify.js
+++ CALL-ME-MAYBE/currify.js
@@ -1,5 +1,6 @@
 function currify(func) {
     return function curried(...args) {
+        console.log(args.length, func.length)
         if (args.length >= func.length) {
             return func.apply(null, args)
         }
@@ -9,9 +10,9 @@ function currify(func) {
     }
 }
 
-// const mult2 = (el1, el2) => el1 * el2
-// console.log(mult2(2, 2)) // result expected 4
+const mult2 = (el1, el2) => el1 * el2
+console.log(mult2(2, 2)) // result expected 4
 
-// const mult2Curried = currify(mult2)
+const mult2Curried = currify(mult2)
 
-// console.log(mult2Curried(2)(2))
+console.log(mult2Curried(2)(2))
