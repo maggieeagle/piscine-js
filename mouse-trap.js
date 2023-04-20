abd729ba5157ec08b0519e663e37bd5372792dc6
maggieeagle <oryol.margo@gmail.com>
2023-04-21 00:32:46 +0300

    .

--- mouse-trap.js
+++ mouse-trap.js
@@ -24,8 +24,8 @@ export const moveCircle = () => {
         let trap = box.getBoundingClientRect();
         let top = y-e.offsetHeight/2, right = x + e.offsetWidth/2, bottom = y + e.offsetHeight/2, left = x-e.offsetWidth/2
         if (!e.classList.contains('trapped')) return true
-        else if (top > trap.top + 1 && left > trap.left &&
-            bottom < trap.bottom && right < trap.right) return true
+        else if (top > trap.top+1 && left > trap.left &&
+            bottom < trap.bottom-1 && right < trap.right) return true
         return false
     }
 }
@@ -45,7 +45,7 @@ function trap(e, box) {
     let trap = box.getBoundingClientRect();
     let circle = e.getBoundingClientRect();
     if (circle.top > trap.top + 1 && circle.left > trap.left &&
-        circle.bottom < trap.bottom && circle.right < trap.right) {
+        circle.bottom < trap.bottom-1 && circle.right < trap.right) {
         e.classList.add('trapped')
         e.style.backgroundColor = getComputedStyle(document.body).getPropertyValue('--purple')
     }
