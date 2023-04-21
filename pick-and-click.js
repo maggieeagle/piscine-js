ddbc072fea7684e22ae8c2f7e24afae715aec91e
maggieeagle <oryol.margo@gmail.com>
2023-04-22 00:27:15 +0300

    .

--- pick-and-click.js
+++ pick-and-click.js
@@ -3,7 +3,7 @@ export const pick = () => {
     document.addEventListener('mousemove', (e) => {
         let value = Math.round(360 / window.innerWidth * e.pageX)
         let luminosity = Math.round(100 / window.innerHeight * e.pageY)
-        hsl = 'hsl(' + value + ', 100%, ' + luminosity + '%)'
+        hsl = 'hsl(' + value + ', 50%, ' + luminosity + '%)'
         document.body.style.backgroundColor = hsl
 
         setDiv(hsl, 'hsl', 'bla')
