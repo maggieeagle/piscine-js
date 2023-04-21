dcb505fd673561d1ea207af1809f427eeba44f08
maggieeagle <oryol.margo@gmail.com>
2023-04-21 13:31:11 +0300

    .

--- mouse-trap.js
+++ mouse-trap.js
@@ -15,7 +15,7 @@ export const moveCircle = () => {
     document.addEventListener('mousemove', (e) => {
         let last = document.body.lastChild
         if (last != null) {
-            console.log(e.pageX-last.offsetWidth/2, e.pageY-last.offsetHeight/2)
+            console.log(e.pageX-last.style.width/2, e.pageY-last.style.width/2)
             if (check(last, e.pageX, e.pageY)) setPosition(last, e.pageX-last.offsetWidth/2, e.pageY-last.offsetHeight/2)
             if (!last.classList.contains('trapped')) trap(last, box)
         }
@@ -23,10 +23,10 @@ export const moveCircle = () => {
 
     function check(e, x, y) {
         let trap = box.getBoundingClientRect();
-        let top = y-Math.round(e.offsetHeight/2), right = x + Math.round(e.offsetWidth/2), bottom = y + Math.round(e.offsetHeight/2), left = x-Math.round(e.offsetWidth/2)
+        let top = y-e.offsetHeight/2, right = x + e.offsetWidth/2, bottom = y + e.offsetHeight/2, left = x-e.offsetWidth/2
         if (!e.classList.contains('trapped')) return true
         else if (top > trap.top+1 && left > trap.left &&
-            bottom < trap.bottom && right < trap.right) return true
+            bottom < trap.bottom-1 && right < trap.right) return true
         return false
     }
 }
@@ -45,8 +45,8 @@ function setPosition(e, x, y) {
 function trap(e, box) {
     let trap = box.getBoundingClientRect();
     let circle = e.getBoundingClientRect();
-    if (circle.top > trap.top + 1 && circle.left > trap.left &&
-        circle.bottom < trap.bottom && circle.right < trap.right) {
+    if (circle.top > trap.top+1 && circle.left > trap.left &&
+        circle.bottom < trap.bottom-1 && circle.right < trap.right) {
         e.classList.add('trapped')
         e.style.background = `var(--purple)`;
     }
