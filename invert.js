a034f33c03c919eb8acc80a4e2157f162a2f0f7e
maggieeagle <oryol.margo@gmail.com>
2023-04-24 11:49:30 +0300

    .

--- invert.js
+++ invert.js
@@ -1,9 +1,7 @@
 function invert(obj) {
-    const entries = Object.entries(obj), res = {}
-    for (let i = entries.length-1; i >= 0; i--) {
-        res[entries[i][1]] = entries[i][0]
-    }
-    return res
+    let arr = Object.entries(obj).map(([key,value])=>[value,key])
+    console.log(arr)
+    return Object.fromEntries(arr)
 }
 
-// console.log(invert({1:'hey', 2:'Jude'}))
+console.log(invert({ brand: 'ford', motor: 'v8', year: 2000, fast: true, eco: true }))
