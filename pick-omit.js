ed36a70c3fbef86324598bb89bb617dc50f5e19f
maggieeagle <oryol.margo@gmail.com>
2023-04-24 14:05:18 +0300

    .

--- pick-omit.js
+++ pick-omit.js
@@ -1,17 +1,11 @@
 function pick(obj, keys) {
-    let string
-    if (typeof keys != 'string') string = keys.join(',')
-    else string = keys
-    let arr = Object.entries(obj).filter(pair => string.includes(pair[0]))
+    let arr = Object.entries(obj).filter(pair => typeof keys != 'string'?keys.join(',').includes(pair[0]):keys==pair[0])
     return Object.fromEntries(arr)
 }
 
 function omit(obj, keys) {
-    let string
-    if (typeof keys != 'string') string = keys.join(',')
-    else string = keys
-    let arr = Object.entries(obj).filter(pair => !string.includes(pair[0]))
+    let arr = Object.entries(obj).filter(pair => typeof keys != 'string'?!keys.join(',').includes(pair[0]):keys!=pair[0])
     return Object.fromEntries(arr)
 }
 
-// console.log(pick({1:'hey', 2:'Jude', 3:'how', four:'are'}, ['four', '2']))
+console.log(pick({age: 32, ageVerified: false}, 'age'))
