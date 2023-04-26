93260cf84ae1320fd004cd4ccd04269f2aedfe50
maggieeagle <oryol.margo@gmail.com>
2023-04-26 23:39:15 +0300

    .

---
+++ flagger.js
@@ -0,0 +1,17 @@
+function flags(obj) {
+    let output = {alias: {h: 'help'}, description: []}
+    Object.entries(obj).forEach(entry => {
+        if(entry[0] != 'help') {
+            output.alias[entry[0][0]] = entry[0]
+            output.description.push('-'+entry[0][0]+', --'+entry[0]+': '+entry[1])
+        }
+    });
+    output.description = output.description.join('\n')
+    return output
+}
+
+// console.log(flags({
+//     multiply: 'multiply the values',
+//     divide: 'divides the values',
+//     help: ['divide']
+// }))
