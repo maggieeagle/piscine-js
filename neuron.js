4c7b5c79cf41f3561512bcd2dbd2330b19b33072
maggieeagle <oryol.margo@gmail.com>
2023-04-26 22:27:03 +0300

    .

---
+++ neuron.js
@@ -0,0 +1,36 @@
+function neuron(arr) {
+    let output = {}
+    for (i in arr) {
+        console.log(arr[i])
+        let re = /^(\w*): ([\w|\s|?|!|,]*) - (\w*): ([\w|\s|?|!|,]*)/
+        let match = arr[i].match(re)
+        let key1 = match[1].toLowerCase(), key2 = removeSign(match[2].toLowerCase()),
+            key3 = key1.slice(0, -1)
+        if (output[key1] == undefined) {
+            output[key1] = { [key2]: { [key3]: match[2], 'responces': [match[4]] } }
+        } else {
+            if (output[key1][key2] == undefined) {
+                output[key1][key2] = { [key3]: match[2], 'responces': [match[4]] }
+            }
+            else {
+                output[key1][key2].responces.push(match[4])
+            }
+        }
+    }
+    return output
+}
+
+function removeSign(str) {
+    if (str.slice(-1) === "?" || str.slice(-1) === "!") {
+        str = str.slice(0, -1);
+    }
+    return str
+}
+
+// console.log(neuron([
+//     'Questions: what is ounces? - Response: Ounce, unit of weight in the avoirdupois system',
+//     'Questions: what is ounces? - Response: equal to 1/16 pound (437 1/2 grains)',
+//     'Questions: what is Mud dauber - Response: Mud dauber is a name commonly applied to a number of wasps',
+//     'Orders: shutdown! - Response: Yes Sr!',
+//     'Orders: Quote something! - Response: Pursue what catches your heart, not what catches your eyes.'
+// ]))
