77d60da6a528aa97f7ca5658c3968574b6001552
maggieeagle <oryol.margo@gmail.com>
2023-04-27 22:14:55 +0300

    .

--- debounce.js
+++ debounce.js
@@ -3,15 +3,15 @@ function debounce(func, delay) {
     return function (...args) {
         clearTimeout(timerId)
         timerId = setTimeout(() => {
-            func
+            func.apply(this, args)
         }, delay);
     }
 }
 
-function opDebounce(func, delay, leading = false) {
-    if (!leading) debounce(func, delay)
-    else return func
-}
+// function opDebounce(func, delay, leading = false) {
+//     if (!leading) debounce(func, delay)
+//     else return func
+// }
 
 // console.log(await Promise.all([
 //     run(debounce(add, 50), { delay: 100, count: 5 }),
