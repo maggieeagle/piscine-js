2f1fd6185a3ea940caef1c3d5ebb4e25a8fd5621
maggieeagle <oryol.margo@gmail.com>
2023-04-28 15:39:55 +0300

    .

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,15 +1,15 @@
 function retry(count, callback) {
-    return async function(...args) {
+    // return async function(...args) {
         let retries = 0
         while (retries <= count) {
             try {
-                return await callback(args).join('')
+                return await callback(args)
             } catch (error) {
                 retries++
                 if (retries > count) throw new Error()
             }
         }
-    }
+    // }
 }
 
 function timeout(delay, callback) {
