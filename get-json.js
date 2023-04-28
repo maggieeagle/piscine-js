32d8f9117abf4c0e3c528d9be26b5fee3b15d104
maggieeagle <oryol.margo@gmail.com>
2023-04-28 21:35:12 +0300

    .

---
+++ get-json.js
@@ -0,0 +1,21 @@
+function getJSON(path, params) {
+    path = appendQuery(path, params)
+    const response = await fetch(url).then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(data => {
+        return await data.json();
+      })
+      .catch(error => {
+        return {'error': error}
+      });
+}
+
+function appendQuery(url, params) {
+    Object.entries(params).forEach(([key, value]) => {
+        url.searchParams.set(key, value);
+    });
+}
