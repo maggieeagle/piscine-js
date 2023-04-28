ac9ddec6cbe8720d2a626216fb52ffdb7658c225
maggieeagle <oryol.margo@gmail.com>
2023-04-28 13:10:53 +0300

    .

--- throttle.js
+++ throttle.js
@@ -16,39 +16,36 @@ function throttle(func, wait) {
 }
 
 function opThrottle(func, wait, options) {
-    let lastCall = 0, timerId = null;
+    let lastCall = 0, timerId = null, lastArgs = null;
     const defaultOptions = { leading: false, trailing: false };
     options = Object.assign({}, defaultOptions, options);
     // console.log(options)
     return function (...args) {
         let now = Date.now();
-        let elapsed = now - (lastCall+wait);
+        let elapsed = now - lastCall;
 
-        if (elapsed >= 0) {
+        if (elapsed >= wait) {
             clearTimeout(timerId);
             lastCall = now;
             func.apply(this, args);
-            if (options.trailing) {
-                timerId = setTimeout(() => {
-                    lastCall = now;
-                    func.apply(this, args);
-                    timerId = null;
-                }, wait-elapsed);
-            }
+            lastArgs = null;
         } else {
             if (options.leading && lastCall === 0) {
                 lastCall = now;
                 func.apply(this, args);
-            } 
-                if (options.trailing && !timerId) {
+                lastArgs = null;
+            }
+            if (options.trailing) {
+                lastArgs = args;
+                if (!timerId) {
                     timerId = setTimeout(() => {
-                        lastCall = now;
-                        elapsed = now - (lastCall+wait);
-                        func.apply(this, args);
+                        lastCall = Date.now();
+                        func.apply(this, lastArgs);
+                        lastArgs = null;
                         timerId = null;
-                    }, wait-elapsed);
+                    }, wait - elapsed);
                 }
-            
+            }
         }
     }
 }
