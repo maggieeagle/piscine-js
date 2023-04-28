4fdcf0e0f4bb0fbdef379b89ee2ceecd6033f7a4
maggieeagle <oryol.margo@gmail.com>
2023-04-28 14:18:29 +0300

    .

--- interpolation.js
+++ interpolation.js
@@ -1,8 +1,11 @@
-function interpolation(obj) {
-    const stepValue = (obj.end - obj.start) / obj.step, timeout = obj.duration / obj.step
-    for (let i = 0; i < obj.step; i++) {
-        setTimeout(() => {
-            obj.callback([stepValue*i, timeout*(i+1)])
-        }, timeout)
+function interpolation(obj, step = 0) {
+    if (currentStep >= obj.step) {
+        return;
     }
+    const stepValue = (obj.end - obj.start) / obj.step
+    const timeout = obj.duration / obj.step
+    setTimeout(() => {
+        obj.callback([stepValue * step, timeout * (step + 1)])
+        interpolation(obj, step + 1);
+    }, timeout)
 }
