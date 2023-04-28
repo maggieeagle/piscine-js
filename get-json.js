8dc950e57bfdcf029c1f1777b806132a4bc4bbbd
maggieeagle <oryol.margo@gmail.com>
2023-04-29 00:39:18 +0300

    .

--- get-json.js
+++ get-json.js
@@ -2,7 +2,7 @@ async function getJSON(path, params) {
     console.log(path)
     let url = path
     url += '?'
-    Object.entries(params).forEach(([key, value]) => url += key + '=' + value.toString().replace('_', '+') + '&')
+    Object.entries(params).forEach(([key, value]) => url += key + '=' + value.toString().replace(' ', '+') + '&')
     url = url.slice(0, -1)
     // const response = await fetch(path)
 
