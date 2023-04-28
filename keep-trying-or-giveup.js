e617a3e17fb3d64a5e50aa3fae00e50d67b48184
maggieeagle <oryol.margo@gmail.com>
2023-04-28 17:26:51 +0300

    .

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,9 +1,9 @@
-function retry(count, callback) {
+async function retry(count, callback) {
     return async function(...args) {
         let retries = 0
         while (retries <= count) {
             try {
-                return await Promise.race(callback(args))
+                return await callback(...args)
             } catch (error) {
                 retries++
                 if (retries > count) throw new Error()
@@ -15,3 +15,11 @@ function retry(count, callback) {
 function timeout(delay, callback) {
 
 }
+
+export const setup = () => ({
+    r: Math.random().toString(36).slice(2),
+    failNTimes: (n) => async (...v) =>
+      --n < 0 ? v : Promise.reject(Error(`x:${v}`)),
+    delayed: (delay) => (...v) => new Promise((s) => setTimeout(s, delay, v)),
+  })
+  
