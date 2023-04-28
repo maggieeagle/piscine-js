731480ba3badfc24d05fde192d89eafb65bacad0
maggieeagle <oryol.margo@gmail.com>
2023-04-28 14:54:57 +0300

    .

---
+++ keep-trying-or-giveup.js
@@ -0,0 +1,13 @@
+function retry(count, callback) {
+    return function(...args) {
+        let retries = 0
+        while (retries <= count) {
+            try {
+                return await callback(args)
+            } catch (error) {
+                retries++
+                if (retries > count) throw new Error()
+            }
+        }
+    }
+}
