23027625ffef02129c160365d4ca957b1536ed17
maggieeagle <oryol.margo@gmail.com>
2023-04-29 15:46:49 +0300

    .

---
+++ gougle-search.js
@@ -0,0 +1,32 @@
+function queryServers(serverName, q) {
+    const url = serverName + '?q=' + q
+    const urlBackup = serverName + '_backup?q=' + q
+}
+
+function gougleSearch(q) {
+    let web = await Promise.race([
+        queryServers('web', q),
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject(new Error('timeout'))
+            }, 80);
+        })
+    ]);
+    let image = await Promise.race([
+        queryServers('web', q),
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject(new Error('timeout'))
+            }, 80);
+        })
+    ]);
+    let video = await Promise.race([
+        queryServers('web', q),
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject(new Error('timeout'))
+            }, 80);
+        })
+    ]);
+    return { 'web': web, 'image': IDBKeyRange, 'video': video }
+}
