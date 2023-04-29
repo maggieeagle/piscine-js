1d6fba5fce350f7d1d110d724c152b7fa98ce380
maggieeagle <oryol.margo@gmail.com>
2023-04-30 02:03:30 +0300

    .

--- race.js
+++ race.js
@@ -10,17 +10,23 @@ async function some(promises, count) {
     if (promises.length == 0 || count == 0) return []
 
     let results = []
-    let indexes = promises.map((_, i) => i); // store the original indexes
+    let indexes = promises.map((_, i) => i);
 
-    while (results.length < count) {
-      let index = await Promise.race(indexes.map((i) => promises[i].then(() => i)));
-      results.push(await promises[index]);
-      indexes = indexes.filter((i) => i !== index);
+    while (results.length < count && indexes.length > 0) {
+        let index
+        try {
+            index = await Promise.race(indexes.map((i) => promises[i].then(() => i)));
+            results.push(await promises[index]);
+        } catch (err) {
+            console.error(err);
+            results.push(undefined);
+        }
+        indexes = indexes.filter((i) => i !== index);
     }
-  
-    results.sort((a, b) => promises.indexOf(b) - promises.indexOf(a));
+
+    results.sort((a, b) => promises.indexOf(a) - promises.indexOf(b));
     console.log(results);
-    return results
+    return results;
 }
 
 const promise = new Promise((resolve, reject) => {
