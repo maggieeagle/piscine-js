618438fd4a07aad19154a930880c8bea543ab649
maggieeagle <oryol.margo@gmail.com>
2023-04-30 02:00:42 +0300

    .

--- race.js
+++ race.js
@@ -8,31 +8,23 @@ async function race(promises) {
 
 async function some(promises, count) {
     if (promises.length == 0 || count == 0) return []
-    // let res = []
-    // console.log(typeof promises)
-    // for (let i = 0; i < count; i++) {
-    //     res.push(race([promises[i]]))
-    // }
 
-    // let all = await Promise.all(promises)
-    // console.log(all)
-    // return all.slice(0, count)
     let results = []
-
     let indexes = promises.map((_, i) => i); // store the original indexes
-  
+
     while (results.length < count) {
       let index = await Promise.race(indexes.map((i) => promises[i].then(() => i)));
-      results[index] = await promises[index];
+      results.push(await promises[index]);
       indexes = indexes.filter((i) => i !== index);
     }
   
+    results.sort((a, b) => promises.indexOf(b) - promises.indexOf(a));
     console.log(results);
-    return results;
+    return results
 }
 
-// const promise = new Promise((resolve, reject) => {
-//     // This promise will never resolve because the resolve function is never called
-// });
+const promise = new Promise((resolve, reject) => {
+    // This promise will never resolve because the resolve function is never called
+});
 
-// console.log(some([Promise.resolve(2), Promise.resolve(5), promise], 2))
+console.log(some([Promise.resolve(2), Promise.resolve(5), promise], 2))
