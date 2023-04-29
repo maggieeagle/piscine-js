b8d6744584e86cd2c85f3296609618633f65b799
maggieeagle <oryol.margo@gmail.com>
2023-04-30 02:11:12 +0300

    .

--- race.js
+++ race.js
@@ -10,28 +10,29 @@ async function some(promises, count) {
     if (promises.length == 0 || count == 0) return []
 
     let results = []
-    let resolvedValues = new Array(promises.length).fill(null);
     let indexes = promises.map((_, i) => i);
 
-    while (results.length < count && indexes.length > 0) {
-        let index
-        try {
-            index = await Promise.race(indexes.map((i) => promises[i].then(() => i)));
-            resolvedValues[index] = await promises[index];
-        } catch (err) {
-            console.error(err);
-        }
-        indexes = indexes.filter((i) => i !== index);
+while (results.length < count && indexes.length > 0) {
+    let index;
+    try {
+        index = await Promise.race(indexes.map((i) => promises[i].then(() => i)));
+        results[index] = await promises[index];
+    } catch (err) {
+        console.error(err);
+        results[index] = undefined;
     }
+    indexes.splice(indexes.indexOf(index), 1);
+}
 
-    for (let i = 0; i < resolvedValues.length && results.length < count; i++) {
-        if (resolvedValues[i] !== null) {
-            results.push(resolvedValues[i]);
-        }
+let orderedResults = [];
+for (let i = 0; i < promises.length; i++) {
+    if (results[i] !== undefined) {
+        orderedResults.push(results[i]);
     }
+}
 
-    console.log(results);
-    return results;
+console.log(orderedResults);
+return orderedResults;
 }
 
 const promise = new Promise((resolve, reject) => {
