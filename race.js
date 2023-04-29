6a1431e05c0148a2608e117e8f60664eb607e0b3
maggieeagle <oryol.margo@gmail.com>
2023-04-30 00:54:30 +0300

    .

---
+++ race.js
@@ -0,0 +1,17 @@
+async function race(promises) {
+    return new Promise((resolve, reject) => {
+        for (let promise of promises) {
+            promise.then(resolve).catch(reject);
+        }
+    });
+}
+
+async function some(promises, count) {
+    if (promises == [] || count == 0) return undefined
+    let res = []
+    for (let i = 0; i < count; i++) {
+        res.push(race(promises))
+        promises.splice(promises.indexOf(first, 1))
+    }
+    return res
+}
