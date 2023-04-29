6641c93a7c7cfe556aead837b29f1e7ca2082775
maggieeagle <oryol.margo@gmail.com>
2023-04-29 15:48:18 +0300

    .

--- gougle-search.js
+++ gougle-search.js
@@ -1,7 +1,23 @@
 function queryServers(serverName, q) {
     const url = serverName + '?q=' + q
     const urlBackup = serverName + '_backup?q=' + q
+    return getFastestJSON(url, urlBackup)
 }
+
+function getFastestJSON(url, urlBackup) {
+    const startTime1 = performance.now();
+    const promise1 = getJSON(url);
+    const endTime1 = performance.now();
+  
+    const startTime2 = performance.now();
+    const promise2 = getJSON(urlBackup);
+    const endTime2 = performance.now();
+  
+    const time1 = endTime1 - startTime1;
+    const time2 = endTime2 - startTime2;
+  
+    return time1 < time2 ? promise1 : promise2;
+  }
 
 function gougleSearch(q) {
     let web = await Promise.race([
