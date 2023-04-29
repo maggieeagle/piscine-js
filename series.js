4e4510759de456a81c149d94a62334acf65f8067
maggieeagle <oryol.margo@gmail.com>
2023-04-29 21:45:40 +0300

    .

--- series.js
+++ series.js
@@ -1,7 +1,7 @@
 function series(asyncFunctions) {
     let res = []
     asyncFunctions.forEach(func => {
-        let output = await func()
+        let output = func()
         res.push(output)
     });
     return res
