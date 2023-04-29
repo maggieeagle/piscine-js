ecda8f39644aba5c0db21144f86a4c6afd949861
maggieeagle <oryol.margo@gmail.com>
2023-04-29 15:07:38 +0300

    .

--- is-winner.js
+++ is-winner.js
@@ -1,14 +1,13 @@
 async function isWinner(country) {
     try {
-    let winner = await db.getWinner(str);
-    if (winner.continent !== 'Europe') {
+    let countryInfo = await db.getWinner(str);
+    if (countryInfo.continent !== 'Europe') {
         return `${country} is not what we are looking for because of the continent`
       }
       const countryResults = await db.getResults(countryInfo.id)
       if (countryResults.length < 3) {
         return `${country} is not what we are looking for because of the number of times it was champion`
       }
-      console.log(countryResults.length)
       const years = countryResults.map((result) => result.year).join(', ')
       const results = countryResults.map((result) => result.score).join(', ')
       return `${country} won the FIFA World Cup in ${years} winning by ${results}`
