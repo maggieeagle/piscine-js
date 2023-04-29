f752f7d5d9de3a5f414b58046f4759142f2bb822
maggieeagle <oryol.margo@gmail.com>
2023-04-29 22:08:56 +0300

    .

--- series.js
+++ series.js
@@ -1,7 +1,10 @@
 async function series(asyncFunctions) {
-    let promises = asyncFunctions.map(func => await func());
+    let promises = asyncFunctions.map(func => {
+        let value = await func()
+        return value
+    });
     let results = await Promise.all(promises);
     return results;
-  }
+}
 
 // console.log(awaitseries([() => Promise.resolve(1), () => Promise.resolve(true)]))
