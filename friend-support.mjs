b596cce8df2648e79df36e935e717810c058c459
maggieeagle <oryol.margo@gmail.com>
2023-04-30 21:40:54 +0300

    .

--- friend-support.mjs
+++ friend-support.mjs
@@ -8,36 +8,21 @@ const server = http.createServer(async function (req, res) {
     if (req.method === 'GET') {
         let name = req.url.slice(1, req.url.length)
         try {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            await readFileAsync('./guests/' + name + '.json')
-            res.end(body);
-            nonExistentFunction();
-        } catch (err) {
-            console.log('listening', server.listening)
-            if (!server.listening) {
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                body = { error: "server failed" };
-                res.end(JSON.stringify(body));
+            const files = await readdir('./guests/');
+            // console.log(files)
+            if (files.indexOf(name) == -1) {
+                res.writeHead(404, { 'Content-Type': 'application/json' });
+                body = { error: "guest not found" }
             } else {
-                const files = await readdir('./guests/');
-                // console.log(files)
-                if (files.indexOf(name) == -1) {
-                    res.writeHead(404, { 'Content-Type': 'application/json' });
-                    body = { error: "guest not found" }
-                }
-                else {
-                    // res.writeHead(500, { 'Content-Type': 'application/json' });
-                    // body = { error: "server failed" }
-                    throw new Error("Internal server error");
-                }
-                res.end(JSON.stringify(body));
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                await readFileAsync('./guests/' + name + '.json')
             }
+            res.end(body);
+        } catch (err) {
+            res.writeHead(500, { 'Content-Type': 'application/json' });
+            body = { error: "server failed" }
+            res.end(body);
         }
-    } else {
-        // If the request is not a GET request with the URL "/", send a 404 response
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        body = { error: "no page found" }
-        res.end(JSON.stringify(body));
     }
 });
 
