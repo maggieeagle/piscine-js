5bde0689f02ca9bad8f87860bf6df23071a2aff7
maggieeagle <oryol.margo@gmail.com>
2023-04-30 18:27:47 +0300

    .

--- friend-support.mjs
+++ friend-support.mjs
@@ -1,21 +1,27 @@
 import * as http from 'node:http'
 import { readFile } from 'fs';
+import { readdir } from 'node:fs/promises';
 
 const server = http.createServer();
 let body
 
-server.on('request', async function(req, res) {
+server.on('request', async function (req, res) {
     if (req.method === 'GET') {
+        let name = req.url.slice(1, req.url.length)
         try {
-            let name = req.url.slice(1, req.url.length)
             res.writeHead(200, { 'Content-Type': 'application/json' });
-            await readFileAsync(name + '.json')
+            await readFileAsync('./guests/' + name + '.json')
             res.end(body);
         } catch (err) {
-            console.log(err)
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            if(err.code = 'ENOENT')  body = { error: "guest not found" }
-            else body = { error: "server failed" }
+            const files = await readdir('/');
+            if (files.indexOf(name) == -1) {
+                res.writeHead(404, { 'Content-Type': 'application/json' });
+                body = { error: "guest not found" }
+            }
+            else {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                body = { error: "server failed" }
+            }
             res.end(JSON.stringify(body));
         }
     } else {
@@ -30,13 +36,13 @@ server.listen(5000, () => console.log(`The server is listening on port 5000`));
 
 async function readFileAsync(file) {
     return new Promise((resolve, reject) => {
-      readFile(file, 'utf8', (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          body = data
-          resolve();
-        }
-      });
+        readFile(file, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                body = data
+                resolve();
+            }
+        });
     });
-  }
+}
