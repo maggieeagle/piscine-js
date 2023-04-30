0bc1fc54b61e756566d1717047de3c40b6ab5959
maggieeagle <oryol.margo@gmail.com>
2023-04-30 14:51:32 +0300

    .

---
+++ tell-me-how-many.mjs
@@ -0,0 +1,7 @@
+let path = process.argv[2] == undefined ? './' : process.argv[2]
+
+import { readdir } from 'node:fs/promises';
+const files = await readdir(path);
+let count = 0
+for (const file of files) count++
+console.log(count-1)
