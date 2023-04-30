0e36fdad1d7827367ca7828bb2a4ba9f768c9a49
maggieeagle <oryol.margo@gmail.com>
2023-05-01 00:23:45 +0300

    .

--- uninvited.mjs
+++ uninvited.mjs
@@ -14,6 +14,13 @@ const server = http.createServer(async function (req, res) {
 
         req.on('end', () => {
             try {
+                try {
+                    JSON.parse(str);
+                } catch (e) {
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    body = { error: "server failed" }
+                    res.end(JSON.stringify(body));
+                }
                 writeToFile(requestBody, './guests/' + name + '.json')
                 res.writeHead(201, { 'Content-Type': 'application/json' });
                 res.end(requestBody);
