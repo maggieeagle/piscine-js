c37c47f0e34879bcc2b980dea915bb883a53f6e3
maggieeagle <oryol.margo@gmail.com>
2023-04-30 14:41:03 +0300

    .

--- verydisco-forever.mjs
+++ verydisco-forever.mjs
@@ -1,5 +1,4 @@
 let words = process.argv[2].replaceAll(' ', ' . ').split(' ')
-let splitWords = []
 words = words.map((word) => {
     let middle = Math.ceil(word.length / 2)
     return [word.slice(middle, word.length), word.slice(0, middle)]
---
+++ verydisco-reverso.mjs
@@ -0,0 +1,10 @@
+import { readFile } from 'fs';
+readFile('verydisco.txt', 'utf8', function(err, words){
+    words = words.replaceAll(' ', ' . ').split(' ')
+    words = words.map((word) => {
+        let middle = Math.floor(word.length / 2)
+        return [word.slice(middle, word.length), word.slice(0, middle)]
+    })
+    words = words.flat().join('').replaceAll('.', ' ')
+    console.log(words)
+});
