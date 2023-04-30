36b0d9bca8e9dde6a4886cc2df72b3c65a722afb
maggieeagle <oryol.margo@gmail.com>
2023-04-30 14:22:00 +0300

    .

---
+++ verydisco-forever.mjs
@@ -0,0 +1,12 @@
+let words = process.argv[2].replaceAll(' ', ' . ').split(' ')
+let splitWords = []
+words = words.map((word) => {
+    let middle = Math.ceil(word.length / 2)
+    return [word.slice(middle, word.length), word.slice(0, middle)]
+})
+const content = words.flat().join('').replaceAll('.', ' ')
+
+import { writeFile } from 'fs/promises';
+writeFile('./test.txt', content, err => {
+  if (err) console.log(err)
+});
