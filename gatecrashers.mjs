fe8b2e6275c3990f9cb94a062140c093480a9773
maggieeagle <oryol.margo@gmail.com>
2023-05-01 02:46:26 +0300

    .

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -40,7 +40,7 @@ const server = http.createServer((req, res) => {
                     } else {
                         // If the file was successfully written
                         res.writeHead(200, { 'Content-Type': 'application/json' });
-                        res.end(body);
+                        res.end(JSON.stringify(body));
                     }
                 });
             } catch (error) {
