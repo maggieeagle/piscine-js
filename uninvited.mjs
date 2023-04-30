7df87da3b597ec900e4d5e0e4613273316adc384
maggieeagle <oryol.margo@gmail.com>
2023-05-01 01:16:12 +0300

    .

--- uninvited.mjs
+++ uninvited.mjs
@@ -5,32 +5,38 @@ let body
 
 const server = http.createServer(async function (req, res) {
     if (req.method === 'POST') {
-        let name = req.url.slice(1, req.url.length)
-
-        let requestBody = '';
-        req.on('data', (chunk) => {
-            requestBody += chunk;
-        });
-
-        req.on('end', () => {
-            try {
-                    await writeToFile(requestBody, './guests/' + name + '.json')
-                    res.writeHead(201, { 'Content-Type': 'application/json' });
-                    res.end(requestBody);
-            } catch (err) {
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                body = { error: "server failed" }
-                res.end(JSON.stringify(body));
-            }
-        });
-
+      let name = req.url.slice(1, req.url.length);
+  
+      let requestBody = '';
+      req.on('data', (chunk) => {
+        requestBody += chunk;
+      });
+  
+      req.on('end', async () => {
+        try {
+          await writeToFile(requestBody, './guests/' + name + '.json');
+          res.writeHead(201, { 'Content-Type': 'application/json' });
+          res.end(requestBody);
+        } catch (err) {
+          console.error(err);
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          const body = { error: "server failed" };
+          res.end(JSON.stringify(body));
+        }
+      });
     }
-});
-
-server.listen(5000, () => console.log(`The server is listening on port 5000`));
-
-function writeToFile(content, filename) {
-    writeFile(filename, content, err => {
-        if (err) console.log(err)
+  });
+  
+  server.listen(5000, () => console.log(`The server is listening on port 5000`));
+  
+  function writeToFile(content, filename) {
+    return new Promise((resolve, reject) => {
+      writeFile(filename, content, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
     });
-}
+  }
