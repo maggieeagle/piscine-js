e4e0eede77b8f4bb02bbcb3c2a868fe4d739fd7c
maggieeagle <oryol.margo@gmail.com>
2023-05-06 19:25:38 +0300

    .

--- gatecrashers.mjs
+++ gatecrashers.mjs
@@ -172,7 +172,9 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify({ error: 'server failed' }));
           } else {
             // If the file was successfully written
+            
             res.setHeader('Content-Type', 'application/json');
+            res.setHeader('Body', JSON.stringify(body));
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify(body));
           }
